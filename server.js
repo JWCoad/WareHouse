c5f3e0eaca8cccc1e945f91f354695f97d1e5846
Jesse Coad <jessecoad@gmail.com>
2021-06-01 19:11:32 +1000

    started adding code to server js

---
+++ server.js
@@ -0,0 +1,17 @@
+// DEPENDENCIES
+
+const express = require("express");
+
+// tell node to use express
+const app = express();
+
+// which port to use
+const PORT = process.env.PORT || 8080;
+
+//  express app to handle data
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//routes placeholder
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
