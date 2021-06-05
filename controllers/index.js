e8740c70ceac11c311e0a6396a42c8f099c618bb
Jesse Coad <jessecoad@gmail.com>
2021-06-06 02:04:13 +1000

    Added api routes for items

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,11 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const apiRoutes = require('./api');
-const homeRoutes = require('./homeRoutes');
+const apiRoutes = require("./api");
+const homeRoutes = require("./homeRoutes");
+// const itemRoutes = require("./itemRoutes");
 
-router.use('/', homeRoutes);
-router.use('/api', apiRoutes);
+router.use("/", homeRoutes);
+router.use("/api", apiRoutes);
+// router.use("/api", itemRoutes);
 
 module.exports = router;
