c0e25eda0f714d9f4fd19ac6a8f6e6346c084dc0
Will Locket <78286026+WillBill84@users.noreply.github.com>
2021-06-05 13:08:18 +1000

    working version
    fixing errors seeding database,

--- models/index.js
+++ models/index.js
@@ -3,23 +3,13 @@ const Item = require('./item');
 const Category = require('./category');
 
 Category.hasMany(Item, {
-  foreignKey: 'category_id',
+  foreignKey: 'id',
   onDelete: 'CASCADE'
 });
 
 Item.belongsTo(Category, {
-  foreignKey: 'category_id'
+  foreignKey: 'id'
 });
 
 
-User.hasMany(Item, {
-    foreignKey: 'user_id',
-    onDelete: 'CASCADE'
-  });
-  
-  Item.belongsTo(User, {
-    foreignKey: 'user_id'
-  });
-  
-
-module.exports = { User, Category, Item};
+module.exports = { User, Category, Item };
