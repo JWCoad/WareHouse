de90b2b299285fd2ce59474eacceabec2445a34d
Susan Su <suchangau@hotmail.com>
2021-06-05 15:06:06 +1000

    fixed

--- models/index.js
+++ models/index.js
@@ -3,13 +3,14 @@ const Item = require('./item');
 const Category = require('./category');
 
 Category.hasMany(Item, {
-  foreignKey: 'id',
-  onDelete: 'CASCADE'
-});
+  foreignKey: 'category_id'
+})
 
 Item.belongsTo(Category, {
-  foreignKey: 'id'
+  foreignKey: 'category_id'
 });
+
+
 
 
 module.exports = { User, Category, Item };
