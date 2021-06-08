f71dfc0ec0c2330f69cc53a84ecc20e90545396e
Will Locket <78286026+WillBill84@users.noreply.github.com>
2021-06-08 19:20:06 +1000

    get items / add items / get items by id

--- controllers/api/itemRoutes.js
+++ controllers/api/itemRoutes.js
@@ -2,6 +2,61 @@ const router = require('express').Router();
 const { Item } = require('../../models');
 
 
+/* get all items */
+router.get('/', (req, res) => {
+    Item.findAll({
+        attributes: ['id',
+            'name',
+            'material',
+            'color',
+            'price',
+        ],
+        order: [
+            ['id', 'ASC']
+        ],
+
+    })
+        .then(dbItemData => res.json(dbItemData.reverse()))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+
+});
+
+
+
+
+/* get by id */
+router.get('/:id', (req, res) => {
+    Item.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: ['id',
+            'name',
+            'material',
+            'color',
+            'price',
+        ],
+
+
+    })
+        .then(dbItemData => {
+            if (!dbItemData) {
+                res.status(404).json({ message: 'No item found with this id' });
+                return;
+            }
+            res.json(dbItemData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+
+
 /* delete items */
 
 
@@ -10,7 +65,30 @@ const { Item } = require('../../models');
 
 
 
+/* add items */
+router.post('/', (req, res) => {
+    Item.create({
+        name: req.body.name,
+        material: req.body.material,
+        color: req.body.color,
+        price: req.body.price
 
+    })
+
+        .then(dbItemData => {
+            req.save(() => {
+                req.body.name = dbItemData.name;
+                req.body.material = dbItemData.material;
+                req.body.color = dbItemData.color;
+                req.body.price = dbItemData.price;
+
+                res.json(dbItemData);
+
+            });
+        })
+
+    res.redirect('/');
+});
 
 
 module.exports = router;
