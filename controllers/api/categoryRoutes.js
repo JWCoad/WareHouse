6fe3e95c55715f07f0e9674f1154d2efbda37cdb
Susan Su <79045434+SusanSu123@users.noreply.github.com>
2021-06-09 21:46:10 +1000

    Update categoryRoutes.js
    code overlapped

--- controllers/api/categoryRoutes.js
+++ controllers/api/categoryRoutes.js
@@ -2,33 +2,20 @@ const router = require('express').Router();
 const { Category } = require('../../models');
 router.get('/:id', async (req, res) => {
     try {
-
         const getItem = await Category.findOne({
-
-        const getCategory = await Category.findOne({
-
             where: {
                 id: req.params.id
             }
         })
-
 
         if (!getItem) {
             res.status(404).json({ message: 'No item found with this id' });
             return;
         }
         res.json(getItem);
-
-        if (!getCategory) {
-            res.status(404).json({ message: 'No item found with this id' });
-            return;
-        }
-        res.json(getOne);
-
     } catch (err) {
         res.status(500).json(err);
     };
 });
-
 
 module.exports = router;
